012671e1b3fe8095427bc96043aabcfd3a169d95
mj-devilz <mickyjimmy@gmail.com>
2023-12-02 09:23:52 +0000

    made some variable changes - added a player sprite

--- main.cpp
+++ main.cpp
@@ -9,9 +9,19 @@ int main() {
     InitWindow(screenWidth, screenHeight, "Blobbo");
 
     // Getting my map texture 32x32
-    Texture2D map = LoadTexture("tileset/map.png");
+    Texture2D mapTexture = LoadTexture("tileset/map.png");
     // Starting vector of the map
     Vector2 mapPosition{0.0f,0.0f};
+
+    // Getting Player texture and location
+    Texture2D playerTexture = LoadTexture("assets/Slime_Idle.png");
+    // Player position is centre of view
+    Vector2 playerPosition {
+        screenWidth/2.0f,
+        screenHeight/2.0f
+    };
+
+    float imageScale{3.0f};
 
     // Setting the Frames Per Second
     SetTargetFPS(60);
@@ -25,15 +35,16 @@ int main() {
         ClearBackground(RAYWHITE);
 
         // Here goes all the Game Logic
-        //Drawing map to scene 
-        DrawTexture(map,mapPosition.x,mapPosition.y,WHITE);
+        //Drawing map to scene | Map size is too small, going to scale it x 3
+        DrawTextureEx(mapTexture,mapPosition,0.0f,imageScale,WHITE);
 
-
+        DrawTextureEx(playerTexture,playerPosition,0.0f,imageScale,WHITE);
 
 
         // teardown Canvas
         EndDrawing();
     }
+    UnloadTexture(mapTexture);
     CloseWindow();
     return 0;
 }
