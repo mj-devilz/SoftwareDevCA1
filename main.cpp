410bc8bf366615834fcf1db895e72b34ef312c87
mj-devilz <mickyjimmy@gmail.com>
2023-12-02 11:34:14 +0000

    refined animations

--- main.cpp
+++ main.cpp
@@ -21,13 +21,15 @@ int main()
     float imageFrame{4.0f};
 
     // Animation variables
-    float runningTime{}; // updated every frame
-    int animationFrame{}; //frame count 
-    const int maxAnimationFrames{4}; // max number of frames for image
+    float runningTime{};                 // updated every frame
+    int animationFrame{};                // frame count
+    const int maxAnimationFrames{4};     // max number of frames for image
     const float updateTime{1.0f / 8.0f}; // amount of time to pass before updating animation frame (8 times /second)
 
     // Getting Player texture and location
     Texture2D playerTexture = LoadTexture("assets/Slime_Idle.png");
+    Texture2D playerTexture_move = LoadTexture("assets/Slime_Walk.png");
+    Texture2D playerTexture_idle = LoadTexture("assets/Slime_Idle.png");
     // Player position is centre of view
     // screenwidth and height divided by two (get centre of screen)
     // half the first image =  width and height divided by two and multiplied by scale
@@ -55,36 +57,68 @@ int main()
 
         // Updating animation frame
         runningTime += GetFrameTime();
-        if(runningTime >= updateTime){
-            animationFrame++; // increment frame
-            runningTime = 0.0f; //reset running time
-            if(animationFrame > maxAnimationFrames) animationFrame = 0; //reset frame when it reaches ends
+        if (runningTime >= updateTime)
+        {
+            animationFrame++;   // increment frame
+            runningTime = 0.0f; // reset running time
+            if (animationFrame > maxAnimationFrames)
+                animationFrame = 0; // reset frame when it reaches ends
         }
 
         // Drawing player to scene | All images to use imageScale
         // Texture Pro allows character scale and add rectangle from sprite sheet
         Rectangle source{((float)playerTexture.width / imageFrame) * animationFrame, 0.f, (float)playerTexture.width / imageFrame, (float)playerTexture.height / imageFrame};
         Rectangle dest{playerPosition.x, playerPosition.y,
-                       ((float)playerTexture.width / imageFrame) * imageScale,
-                       ((float)playerTexture.height / imageFrame) * imageScale};
-        DrawTexturePro(playerTexture, source, dest, Vector2{},0.0f, WHITE);
+                       (imageScale * (float)playerTexture.width / imageFrame),
+                       (imageScale * (float)playerTexture.height / imageFrame)};
+        DrawTexturePro(playerTexture, source, dest, Vector2{}, 0.0f, WHITE);
+        DrawRectangleLinesEx(dest, 1.0f, RED);
 
         // Adding Movement - moving the map / works with WASD and Arrows
         Vector2 moveDirection{};
-        
-        if(IsKeyDown(KEY_W)||IsKeyDown(KEY_UP)) moveDirection.y -= 1.0f;
-        if(IsKeyDown(KEY_S)||IsKeyDown(KEY_DOWN)) moveDirection.y += 1.0f;
-        if(IsKeyDown(KEY_A)||IsKeyDown(KEY_LEFT)) moveDirection.x -= 1.0f;
-        if(IsKeyDown(KEY_D)||IsKeyDown(KEY_RIGHT)) moveDirection.x += 1.0f;
-        
+
+        if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))
+            moveDirection.y -= 1.0f;
+        if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))
+            moveDirection.y += 1.0f;
+        if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))
+            moveDirection.x -= 1.0f;
+        if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))
+            moveDirection.x += 1.0f;
+
         // vector2length used to see if there is movement
-        if(Vector2Length(moveDirection) != 0.0){
+        if (Vector2Length(moveDirection) != 0.0)
+        {
             // normalized use to make sure diagonal speed is normalized
             // moving the map position based on our movedirection
             // vector2scale allowing us to add a speed factor to our normalized direction
-            mapPosition = Vector2Subtract(mapPosition,Vector2Scale(Vector2Normalize(moveDirection),movementSpeed));
-
+            mapPosition = Vector2Subtract(mapPosition, Vector2Scale(Vector2Normalize(moveDirection), movementSpeed));
             //----ADD CHANGE IN DIRECTION OF IMAGE BASED ON DIRECTION----//
+            if (moveDirection.y < 0)
+            {
+                // Add Left Texture
+                playerTexture = playerTexture_move;
+            }
+            else if (moveDirection.y > 0)
+            {
+                // Add Right Texture
+                playerTexture = playerTexture_move;
+            }
+            else if (moveDirection.y == 0 && moveDirection.x < 0)
+            {
+                // Add Up Texture
+                playerTexture = playerTexture_move;
+            }
+            else if (moveDirection.y == 0 && moveDirection.x > 0)
+            {
+                // Add Down Texture
+                playerTexture = playerTexture_move;
+            }
+        }
+        else
+        {
+            // if direction = 0, then idle animation plays
+            playerTexture = playerTexture_idle;
         }
 
         // teardown Canvas
