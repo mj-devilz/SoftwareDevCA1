cf201d1a169ec00f9441d5215f98d064c47cf87c
mj-devilz <mickyjimmy@gmail.com>
2023-12-03 09:39:55 +0000

    Added Pause / Timer / Win condition

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include "Enemy.h"
 #include <string>
 
+bool paused{false}; //adding global bool variable for pause
+
 int main()
 {
     // Changed window width and height to give a nice square view
@@ -20,9 +22,12 @@ int main()
     Texture2D mapTexture = LoadTexture("tileset/map.png");
     // Starting vector of the map
     Vector2 mapPosition{0.0f, 0.0f};
-
+    // Creating a new enemy class
     Enemy redBlob{Vector2{},RED};
     redBlob.setTarget(&player); // target is the address of the player
+
+    int gameTime{60}; // win condition set - timer 
+    float timer{}; // stores the increment value for the timer
 
     // Setting the Frames Per Second
     SetTargetFPS(60);
@@ -35,6 +40,10 @@ int main()
         BeginDrawing();
         // Clear canvas to a specific color to avoid flicker
         ClearBackground(RAYWHITE);
+        // bool to set true/false value to paused when KEY_P is pressed
+        if(IsKeyPressed(KEY_P)){
+            paused = !paused;
+        }
 
         // Here goes all the Game Logic
         // Need to update the map position based on the characters world position
@@ -43,18 +52,32 @@ int main()
         // Drawing map to scene | Map size scale it x 3
         DrawTextureEx(mapTexture, mapPosition, 0.0f, imageScale, WHITE);
 
-        //If character is caught (isCaught = true), we stop rendering all items and show game over message
-        if(player.getIsCaught()){ //character is caught
-            //------------Add Game Over screen------------//
-            DrawText("You were caught!!!",screenHeight/3,screenWidth/3,44,RED);
-            EndDrawing();
-            continue; // skips to next iteration of while loop.
-        } else { // character alive
-            std::string playersHealth = "Health: ";
-            playersHealth.append(std::to_string(player.getHealth()), 0, 3);
-            DrawText(playersHealth.c_str(),55.0f,40.0f,34.0f,RED);
-            
-        }
+        //if paused returns true, we stop all functions until returns false
+        if(!paused){
+            //creating a timer to decrement the gameTime value
+            timer+=GetFrameTime();
+            if(timer>=1.0f){
+                gameTime--;
+                timer = 0.0f;
+            }
+            //winning condition is to survive until timer is up once won, we stop the game.
+            if(gameTime<=0){
+                DrawText("You win!!!",screenHeight/3,screenWidth/3,44,GREEN);
+                EndDrawing();
+                continue; // skips to next iteration of while loop.
+            }
+            //If character is caught (isCaught = true), we stop rendering all items and show game over message
+            if(player.getIsCaught()){ //character is caught
+                //------------Add Game Over screen------------//
+                DrawText("You were caught!!!",screenHeight/3,screenWidth/3,44,RED);
+                EndDrawing();
+                continue; // skips to next iteration of while loop.
+            } else { // character alive
+                std::string playersHealth = "Timer: ";
+                playersHealth.append(std::to_string(gameTime), 0, 3);
+                DrawText(playersHealth.c_str(),screenWidth*0.6f,20.0f,40.0f,RED);
+                
+            }
             // runs all the functions needed for drawing player / movement /animating player
             player.tick(GetFrameTime());
             // check to see if player goes out of bounds i.e. worldPosition moves off map. (take scaling into account)
@@ -68,6 +91,9 @@ int main()
             }
 
             redBlob.tick(GetFrameTime());
+          } else {
+            DrawText("Paused",screenHeight/3,screenWidth/3,44,GREEN);
+          }
 
             ///added to test character death (keep commented out)
             // if(IsKeyDown(KEY_R)){
