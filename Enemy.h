e0f25ef1803009d7d90487290bdf334c00694425
mj-devilz <mickyjimmy@gmail.com>
2023-12-03 11:58:41 +0000

    removing unwanted variables

--- Enemy.h
+++ Enemy.h
@@ -8,10 +8,9 @@ class Enemy : public BaseCharacter
 public:
     Enemy(Vector2 spawnPosition, struct Color color);
     virtual void tick(float deltaTime) override; // movement and animation from BaseCharacter class
-    void setTarget(Player *player) { target = player; };
+    void setTarget(Player *player) { target = player; }; // uses a pointer to Player to set the value of target
 
 private:
-    Player *target; // pointer to character header
-    float damagePerSecond{20.0f}; // damage the enemy does
+    Player *target; // pointer to player header
     float minDistToPlayer{20.0f}; // distance where enemy stops moving towards player (glitch fix)
 };
--- player.h
+++ player.h
@@ -11,10 +11,8 @@ public:
     Player(int screenWidth, int screenHeight);
     virtual void tick(float deltaTime) override; // movement and animation from BaseCharacter class
     Vector2 getScreenPosition() { return screenPosition; };
-    float getHealth() { return health; } // get health
 
 private:
-    float health{100.0f};      // player health set
     float boostTime{1.5f};     // length of time the boost is active
     float boostCooldown{3.0f}; // cooldown added until boost can be activated again
     bool boostActive{false};   // if boost is active or not
